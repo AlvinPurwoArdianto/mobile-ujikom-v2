24a19f8538b65dc9219f9f8f97736d82d741b030
alvin <alvinpurwo03@gmail.com>
2025-03-12 08:30:07 +0700

    first commit

---
+++ lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: unused_local_variable
+
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:lottie/lottie.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    HomeController controller = Get.put(HomeController());
+    return Scaffold(
+      backgroundColor: HexColor("#34a1eb"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Lottie.asset(
+              'assets/lottie/unscreen.json',
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Aplikasi HRD',
+              style: GoogleFonts.roboto(
+                textStyle: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/app/utils/api.dart
@@ -0,0 +1,4 @@
+class BaseUrl {
+  static String login = 'http://192.168.0.159:8000/api/login';
+  static String logout = 'http://192.168.0.159:8000/api/logout';
+}
